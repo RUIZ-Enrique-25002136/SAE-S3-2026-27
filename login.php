8179d658c9ba7967f2f649fd7b1deedb9d2c5f78
Colib <rossinesleny@gmail.com>
2026-09-18 17:14:35 +0200

    essai

--- login.php
+++ login.php
@@ -1,1 +1,28 @@
 <?php
+$mdp = $_POST['password'];
+$login = $_POST['login'];
+$action = $_POST['action'];
+if($action == "submit"){
+    if (sizeof($mdp) === 0 || sizeof($login) === 0) {
+        return "Veuillez remplir tous les champs";
+    }
+    $link = mysqli_connect('localhost', 'admin', 'admin')
+    or die('Pb de connexion au serveur: ' . mysqli_connect_error());
+    mysqli_select_db($link, 'my_dbname') or die ('Pb de sélection BD : ' . mysqli_error($link));
+    $query = "SELECT user FROM Users WHERE login = $login and pwd = MD5('$mdp') ";
+    $result = mysqli_query($link, $query);
+    if (!$result)
+    {
+        echo 'Impossible d\'exécuter la requête ', $query, ' : ', mysqli_error($link);
+    }
+    else {
+        if (mysqli_num_rows($result) != 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row['username'];
+            }
+        }
+        else{
+        echo "nom d'utilisateur ou mot de passe incorrect";
+        }
+    }
+    }
