2aebef92a00454b822aa1f2fc7a1cbe2af14da3e
Vladislav Dumont <vladislav.dumont@etu.univ-amu.fr>
2026-09-18 17:26:39 +0200

    feat(includes): separation header/footer d'index

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+$utilisateur = isset($_SESSION['utilisateur']) ? $_SESSION['utilisateur'] : null;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+<nav>
+    <a href="index.php">Accueil</a>
+    <?php if ($utilisateur === null): ?>
+        | <a href="login.php">Connexion</a>
+        | <a href="register.php">Inscription</a>
+    <?php else: ?>
+        | <a href="index.php?action=logout">Déconnexion</a>
+    <?php endif; ?>
+</nav>
