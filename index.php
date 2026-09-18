e9fd32ec5c3a0243f36b6111612eb4bdc9f386b5
hugo-banti <hugo.banti@epitech.eu>
2026-09-18 15:59:23 +0200

    feat(index.php) first form

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php ?>
+
+<!DOCTYPE html>
+Hello word !
+<form action="info.php" method="post">
+    <label for="fname">Login</label><br>
+    <input type="text" id="fname" name="login"><br>
+    <label for="lname">password:</label><br>
+    <input type="text" id="lname" name="password"> <br>
+    <button type="submit" value="Submit">Submit</button>
+    <button type="reset" value="Reset">Reset</button>
+</form>
---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+$password = $_POST['password'];
+$login = $_POST['login'];
+
+echo $login;
+echo '<br>';
+echo $password;
+?>
