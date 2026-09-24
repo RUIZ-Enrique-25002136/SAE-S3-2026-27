2338e8d23e5766ab982b4c185b8ce5596e89c934
Colib <rossinesleny@gmail.com>
2026-09-24 16:43:02 +0200

    Correctiftest (#10)
    * fix (login.php, index.php) : Correctif d'affichage des variables et de récupérations.
    
    * Feat (index,login,register) : Fix problème d'affichage

--- index.php
+++ index.php
@@ -17,26 +17,6 @@ $titre = 'Accueil';
 buildHeader();
 ?>
 
-    <body>
-
-    <nav>
-        <a href="index.php">Accueil</a>
-        <?php if ($utilisateur === null): ?>
-            | <a href="login.php">Connexion</a>
-            | <a href="register.php">Inscription</a>
-        <?php else: ?>
-            | <a href="index.php?action=logout">Déconnexion</a>
-        <?php endif; ?>
-    </nav>
-
-    <h1>Bienvenue</h1>
-
-    <?php if ($utilisateur !== null): ?>
-        <p>Bonjour <strong><?= htmlspecialchars($utilisateur['email']) ?></strong>, vous êtes connecté.</p>
-    <?php else: ?>
-        <p>Vous n'êtes pas connecté. <a href="login.php">Connectez-vous</a> ou <a href="register.php">créez un compte</a>.</p>
-    <?php endif; ?>
-
 <h1>Bienvenue</h1>
 
 <?php if ($utilisateur !== null): ?>
